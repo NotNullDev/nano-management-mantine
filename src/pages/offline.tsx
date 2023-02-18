e22b8b65d7ce787de5fc274f8428b5b26e2fe07d
notnulldev <notnulldev0@gmail.com>
2023-02-19 00:15:18 +0100

    ssr quick fix

--- src/pages/offline.tsx
+++ src/pages/offline.tsx
@@ -1,23 +1,22 @@
-import {useEffect, useState} from "react";
+import {useEffect} from "react";
 import {useStore} from "zustand";
 import {userStore} from "@/logic/common/userStore";
 import {useRouter} from "next/router";
 
 export default function OfflinePage() {
-    const online = useOnlineStatus()
     useOnlineBack();
 
     return (
         <pre className="flex-1 justify-center items-center flex flex-col">
+            {/*{*/}
+            {/*    !navigator.onLine && (*/}
+            {/*        <div>Internet access is required to use Nano Management</div>*/}
+            {/*    )*/}
+            {/*}*/}
             {
-                !online && (
-                    <div>Internet access is required to use Nano Management</div>
-                )
-            }
-            {
-                online && (
-                    <div>Sorry, it seems like we have trouble connecting to our servers.</div>
-                )
+                // navigator.onLine && (
+                <div>Sorry, it seems like we have trouble connecting to our servers.</div>
+                // )
             }
             <div>Please try again later.</div>
         </pre>
@@ -34,13 +33,13 @@ const useOnlineBack = () => {
         }
     }, [serverStatus])
 }
-
-const useOnlineStatus = () => {
-    const [online, setOnline] = useState(true)
-
-    useEffect(() => {
-        setOnline(navigator.onLine)
-    }, [navigator.onLine])
-
-    return online;
-}
+//
+// const useOnlineStatus = () => {
+//     const [online, setOnline] = useState(true)
+//
+//     useEffect(() => {
+//         setOnline(navigator.onLine)
+//     }, [])
+//
+//     return online;
+// }
